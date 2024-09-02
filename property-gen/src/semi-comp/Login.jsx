860b5a28b2363ee9a750156b20aaa58d8d6c64a8
sarthak singh <buddysuccess7@gmail.com>
2024-09-02 20:47:50 +0530

    check

--- property-gen/src/semi-comp/Login.jsx
+++ property-gen/src/semi-comp/Login.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginButton = () => {
-  const { loginWithRedirect, user } = useAuth0();
+  const { loginWithRedirect, logout, user, isAuthenticated, error } = useAuth0();
+
+  if (error) {
+    console.error('Auth0 Error:', error);
+  }
   console.log(loginWithRedirect, user,useAuth0, "login");
 
   return (
