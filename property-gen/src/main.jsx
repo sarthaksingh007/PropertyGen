4445f3d21d41a918ac6a86f19a7c3c7cfc99cac2
sarthak singh <buddysuccess7@gmail.com>
2024-09-02 19:26:35 +0530

    done

--- property-gen/src/main.jsx
+++ property-gen/src/main.jsx
@@ -2,22 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { Auth0Provider } from '@auth0/auth0-react';
+import { Auth0Provider } from "@auth0/auth0-react";
 import { ChatbotProvider } from "./chatContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
-    domain="dev-oqeu2nb3dvlee8hw.us.auth0.com"
-    clientId="xPwcGt9L0R5Sd227uFXoZ5Qj9oaYtnJm"
-    authorizationParams={{
-      redirect_uri: window.location.origin
-    }}
-  >
-
-    <ChatbotProvider>
-      <App />
-    </ChatbotProvider>
-  </Auth0Provider>
+      domain="dev-oqeu2nb3dvlee8hw.us.auth0.com"
+      clientId="zMvs6r0MKJ7TJuqSynISYuImYkMlxt1U"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <ChatbotProvider>
+        <App />
+      </ChatbotProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
--- property-gen/src/semi-comp/Login.jsx
+++ property-gen/src/semi-comp/Login.jsx
@@ -3,6 +3,8 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
+  console.log(loginWithRedirect,"login");
+  
 
   return <button className="text-white text-center" onClick={() => loginWithRedirect()}>Log In</button>;
 };
