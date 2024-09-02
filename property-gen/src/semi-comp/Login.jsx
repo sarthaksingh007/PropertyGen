71fa90aa5408c7448c7783edbeedfab8788c71bb
sarthak singh <buddysuccess7@gmail.com>
2024-09-02 20:10:12 +0530

    check

--- property-gen/src/semi-comp/Login.jsx
+++ property-gen/src/semi-comp/Login.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
-  console.log(loginWithRedirect,"login");
-  
+  const { loginWithRedirect, user } = useAuth0();
+  console.log(loginWithRedirect, user, "login");
 
-  return <button className="text-white text-center" onClick={() => loginWithRedirect()}>Log In</button>;
+  return (
+    <button
+      className="text-white text-center"
+      onClick={() => loginWithRedirect()}
+    >
+      Log In
+    </button>
+  );
 };
 
 export default LoginButton;
