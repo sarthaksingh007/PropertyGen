93689834cb0183d2830ff6b2162814b9773686a4
sarthak singh <buddysuccess7@gmail.com>
2024-09-04 11:43:40 +0530

    done with it

--- property-gen/src/main.jsx
+++ property-gen/src/main.jsx
@@ -4,12 +4,14 @@ import App from "./App.jsx";
 import "./index.css";
 import { Auth0Provider } from "@auth0/auth0-react";
 import { ChatbotProvider } from "./chatContext.jsx";
+const auth0Domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const auth0ClientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
-      domain="dev-oqeu2nb3dvlee8hw.us.auth0.com"
-      clientId="zMvs6r0MKJ7TJuqSynISYuImYkMlxt1U"
+      domain={auth0Domain}
+      clientId={auth0ClientId}
       authorizationParams={{
         redirect_uri: window.location.origin + '/chat',
       }}
--- property-gen/src/semi-comp/Hero1.jsx
+++ property-gen/src/semi-comp/Hero1.jsx
@@ -1,7 +1,10 @@
 import Gott from "./../assets/got.png";
-
+import { useAuth0 } from "@auth0/auth0-react";
 import LoginButton from "./Login";
 const Hero1 = () => {
+  const { user } = useAuth0;
+  console.log(user, "in the hero1");
+
   return (
     <div className="herrr">
       <img src={Gott} alt="" className="sm:p-4  sm:w-2/6 w-3/5 p-5  mx-auto" />
