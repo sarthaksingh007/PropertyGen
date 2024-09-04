463d9c298b2a59a7247b04bc52a1eee70a7df512
sarthak singh <buddysuccess7@gmail.com>
2024-09-04 12:16:56 +0530

    done

--- property-gen/src/semi-comp/Hero1.jsx
+++ property-gen/src/semi-comp/Hero1.jsx
@@ -2,8 +2,8 @@ import Gott from "./../assets/got.png";
 import { useAuth0 } from "@auth0/auth0-react";
 import LoginButton from "./Login";
 const Hero1 = () => {
-  const { user } = useAuth0;
-  console.log(user, "in the hero1");
+  const { user,isAuthenticated } = useAuth0;
+  console.log(user,isAuthenticated, "in the hero1");
 
   return (
     <div className="herrr">
@@ -15,38 +15,6 @@ const Hero1 = () => {
           consultant!
         </span>
       </p>
-
-      {/* <div className="my-16">
-        <form
-          // onSubmit={handleSubmit}
-          style={shadowStyle}
-          className={
-            "flex flex-row sm:justify-between justify-center sm:items-start  rounded-full items-center py-2 sm:flex-nowrap flex-wrap sm:w-[98%]  mx-auto w-[70%]   "
-          }
-        >
-          <div className={"flex flex-row justify-between items-center  w-full"}>
-            <div className="flex flex-row justify-between items-center sm:w-[98%]  mx-auto w-[95%]  bg-white rounded-full p-1  border-gray-300 border-2">
-              <input
-                type="text"
-                value={inputText}
-                placeholder={
-                  showplaceholder ? placeholder : "ask me anything..."
-                }
-                className="focus:outline-none w-full rounded-full text-md sm:px-3 p-[0.35rem]"
-                onClick={handleClick}
-                required
-              />
-              <button
-                type="submit"
-                // disabled={isLoading}
-                className="flex justify-center items-center  text-black text-md px-2 rounded-full py-2 relative h-10 sm:text-[10.5px]"
-              >
-                <img src={search} alt="" className="sm:w-10 w-12" />
-              </button>
-            </div>
-          </div>
-        </form>
-      </div> */}
       <LoginButton />
     </div>
   );
